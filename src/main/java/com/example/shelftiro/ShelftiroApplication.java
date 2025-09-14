56e1d92ca37ff9c808a3d2736742b5e87b9b353b
Saifullah Shahid <majesticverse369@gmail.com>
2025-09-14 07:50:37 +0500

    Initial Commit

---
+++ src/main/java/com/example/shelftiro/ShelftiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.shelftiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelftiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelftiroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shelftiro/ShelftiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shelftiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelftiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
